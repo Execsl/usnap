8279db827d53c1af60ce192d83311e2ac3c240d0
Lenik <pub.lenik@bodz.net>
2006-03-16 09:12:01 +0000

    flexible config property, for lazy- connection string

--- vbmod/crypt/src/Digest.cpp
+++ vbmod/crypt/src/Digest.cpp
@@ -9,6 +9,15 @@
 // CDigest
 
 
+static DWORD le_to_be(DWORD x) {
+    WORD h = x >> 16; 
+    WORD l = x & 0xFFFF; 
+    
+    h = (h >> 8) | (h << 8); 
+    l = (l >> 8) | (l << 8); 
+    return h | (l << 16); 
+}
+
 STDMETHODIMP CDigest::MD5_String(BSTR message, long *a_msb, long *b, long *c, long *d_lsb)
 {
     const char *msg = ""; 
@@ -20,16 +29,16 @@ STDMETHODIMP CDigest::MD5_String(BSTR message, long *a_msb, long *b, long *c, lo
     }
     
     int len = strlen(msg); 
-
+    
     MD5 md5impl; 
     md5impl.update((const unsigned char *)msg, len); 
     md5impl.finalize(); 
     
     long *d = (long *)md5impl.raw_digest(); 
-    if (a_msb) *a_msb = d[3]; 
-    if (b) *b = d[2]; 
-    if (c) *c = d[1]; 
-    if (d_lsb) *d_lsb = d[0]; 
+    if (a_msb) *a_msb = le_to_be(d[0]); 
+    if (b) *b = le_to_be(d[1]); 
+    if (c) *c = le_to_be(d[2]); 
+    if (d_lsb) *d_lsb = le_to_be(d[3]); 
     
 	return S_OK;
 }
